fd2fed66344c717406311d9f036716b953510dbd
AnonymousA1025 <110613686+AnonymousA1025@users.noreply.github.com>
2023-11-17 18:14:05 +0530

    Update config.py
    Signed-off-by: AnonymousA1025 <110613686+AnonymousA1025@users.noreply.github.com>

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1001978176529)
+LOGGER_ID = int(getenv("LOGGER_ID", -1001978176529))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6542673293))
